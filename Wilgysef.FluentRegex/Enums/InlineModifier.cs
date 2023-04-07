26b5f5591a65fc5f18dbed1ab16fd60129131abe
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2023-04-07 01:56:41 -0400

    add file

---
+++ Wilgysef.FluentRegex/Enums/InlineModifier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Wilgysef.FluentRegex.Enums
+{
+    [Flags]
+    public enum InlineModifier
+    {
+        /// <summary>
+        /// No modifiers.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Case-insensitive matching.
+        /// </summary>
+        IgnoreCase = 1,
+
+        /// <summary>
+        /// Multi-line. <see cref="AnchorPattern.BeginLine"/> and <see cref="AnchorPattern.EndLine"/> match the beginning and end of lines instead of the entire string.
+        /// </summary>
+        Multiline = 2,
+
+        /// <summary>
+        /// All unnamed capturing groups are treated as non-capturing.
+        /// </summary>
+        ExplicitCapture = 4,
+
+        /// <summary>
+        /// Single-line. The single character pattern matches every character, instead of every character except <c>\n</c>.
+        /// </summary>
+        Singleline = 16,
+
+        /// <summary>
+        /// Ignore unescaped whitespace from the pattern.
+        /// </summary>
+        IgnorePatternWhitespace = 32,
+    }
+}
